9f8c1110a88204e388a790e05a7e611e1083ee59
Davi Staaks <109167350+Staaks77@users.noreply.github.com>
2023-03-16 18:36:40 -0300

    Add files via upload

---
+++ Guess.py
@@ -0,0 +1,41 @@
+# Guess the number
+
+import random
+import time
+
+def numero_escondido():
+
+    dimensao = int(input("\nDefine the limit: "))
+
+    number = random.randint(1, dimensao)
+
+    try_number = 0      # variavel referente a escolha do usuario
+    tries = 3           # variavel tentativas
+    result = 0          # variavel para definir o resultado
+
+    print("\nAre you ready ?\n")
+    time.sleep(1)
+
+    while try_number != number and tries > 0:
+
+        print(f"You have {tries} tries!!!")
+        try_number = int(input(f"Choose a number between 0 and {dimensao}: "))
+
+        if try_number > number:
+            print(f"{try_number} is greater than the guess number\n")
+        elif try_number < number:
+            print(f"{try_number} is less than the guess number\n")
+        elif try_number == number:
+            result = 1
+        else:
+            result = 0
+
+        tries = tries - 1         
+
+    else: 
+        if result == 0:
+            print(f"Oh no :( you lose. The guess number is {number}")
+        elif result == 1:
+            print(f"Congratulations, you guessed the number {number}")
+
+numero_escondido()
